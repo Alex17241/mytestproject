ff7acc74500d132f8d442f5d8d1791c34e46df04
M1ds17 <chenalexey98@gmail.com>
2026-03-06 03:33:20 +0500

    initial commit

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,17 +12,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from django.conf.global_settings import STATICFILES_DIRS
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+from dotenv import load_dotenv
+import os
+
 load_dotenv()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
